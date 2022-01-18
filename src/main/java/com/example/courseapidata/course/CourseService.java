6e1caa13f252292c797dc504b368698265e99d94
Arpan Maity <33843098+binary-greens@users.noreply.github.com>
2022-01-18 19:49:10 +0530

    getAllTopics() modified. The same data can be fetched in the other way by iterating over the iterable and adding each element to the list topics.

---
+++ src/main/java/com/example/courseapidata/course/CourseService.java
@@ -0,0 +1,36 @@
+package com.example.courseapidata.course;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CourseService {
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    public List<Course> getAllCourses(String topicId) {
+        return new ArrayList<>(courseRepository.findByTopicId(topicId));
+    }
+
+    public Optional<Course> getCourse(String id){
+        return courseRepository.findById(id);
+    }
+
+    public void addCourse(Course course) {
+        courseRepository.save(course);
+    }
+
+    public void updateCourse(Course course) {
+        courseRepository.save(course);
+    }
+
+    public void deleteCourse(String id) {
+        courseRepository.deleteById(id);
+    }
+}
