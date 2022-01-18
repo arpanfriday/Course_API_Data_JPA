168a741fa8d7062eaf4767f7c25efcb6b982aa72
Arpan Maity <33843098+binary-greens@users.noreply.github.com>
2022-01-18 19:56:32 +0530

    Method is defined in CourseRepository.java . The description of the method and usage is written in comment.

--- src/main/java/com/example/courseapidata/course/CourseRepository.java
+++ src/main/java/com/example/courseapidata/course/CourseRepository.java
@@ -5,5 +5,15 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface CourseRepository extends CrudRepository<Course, String>{
+//    This is a special function which is not there in the CrudRepository
+//    but if we write the function in such convention then JPA is intelligent enough to
+//    know what it has to do.
+//    public  List<Course> findByName(String name);
+//    public  List<Course> findByDescription(String desc);
+
+//----We use findByTopicId because
+//    findBy is the phrase that are use for applying filters like this
+//    Topic is the class
+//    Id is the attribute of the Topic object
     public List<Course> findByTopicId(String topicId);
 }
